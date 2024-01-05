4499d8c776fa026d2b3b2fe29a531dc428b72f67
Iqra shahzad <141583626+iqrawd@users.noreply.github.com>
2024-01-05 14:13:42 +0500

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,12 @@
+function submitForm() {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    // You can perform further validation and processing here
+
+    alert('Sign Up Successful!\nUsername: ' + username + '\nEmail: ' + email );
+}
+
+
+
